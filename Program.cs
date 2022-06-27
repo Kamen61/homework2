c290fcadb6279f6db93167a838bf27864d6e3e34
Kamen <canen321@gmail.com>
2022-06-27 16:46:06 +0300

    exercise15

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// dz Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// Console.Write("Введите цифру : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number<8 && number>0)
+// {
+//     if (number<6 && number>0)   
+//     {
+
+//     Console.Write("нет");
+//     }
+//  else
+//     {
+//         Console.Write("да");
+//     }
+
+// }
